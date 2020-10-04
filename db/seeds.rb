12a43e8c2f42d75aac6c099e9f8368dfb8678b57
Neil Freeman <freeman.neil92@gmail.com>
2020-10-04 13:20:44 -0400

    added locationseed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,41 +7,68 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Park.create([  
-{
-    name: "Johnny Kelley",    
-    address: "180 Old Bass River Rd, South Dennis, MA", 
-    location: {lat: 41.699520, lng: -70.160019}, 
-    lat: 41.699520, 
-    lng: -70.160019,
-    sports: ["basketball", "soccer", "tennis"],
-    img: "https://3.bp.blogspot.com/-lDkVJCpt1zI/VbyySsK-8XI/AAAAAAAADLE/a8FRZyUOIYw/s1600/Johnny%2BKelley%2BBasketball%2BCourt.jpg"
-},
-{
-    name: "Old Town House",  
-    address: "144 Old Town Hosue Rd, West Yarmouth, MA",      
-    location: {lat: 41.684768, lng: -70.216787 },
-    lat: 41.684768, 
-    lng: -70.216787, 
-    sports: ["basketball", "soccer"],
-    img: "http://4.bp.blogspot.com/_-_T9WYPXZxQ/TD4xYFeXRwI/AAAAAAAAAr0/qz_nLYsXmY8/s1600/071410_1337.JPG"
-},
-{
-    name: "Ezra Baker",    
-    address: "810 Main St, West Dennis, MA 02670", 
-    location: {lat: 41.668521, lng: -70.152941}, 
-    lat: 41.668521, 
-    lng: -70.152941,
-    sports: ["basketball", "soccer", "tennis", "baseball"],
-    img: "http://www.oldstratforduponavon.com/sitebuilder/images/masscapecod-620x413.jpg"
-},
-{
-    name: "Nathaniel Wixon",    
-    address: "901 Rte 134, South Dennis, MA 02660", 
-    location: {lat: 41.714379, lng: -70.156675}, 
-    lat: 41.714379, 
-    lng: -70.156675,
-    sports: ["basketball", "baseball", "football", "tennis"],
-    img: "http://1.bp.blogspot.com/-dq7MtRfdplo/VhkK5UiS-nI/AAAAAAAADVk/MMS0kSeQjwQ/s1600/Wixon%2BSchool%2BPlaying%2BField.jpg"
-}
-
+    
+    # {
+    #     name: "Eastham Park",    
+    #     address: "2500 State Hwy, Eastham, MA", 
+    #     location: {lat: 41.830449, lng: -69.972646}, 
+    #     lat: 41.830449, 
+    #     lng: -69.972646,
+    #     sports: ["baseball", "soccer", "tennis", "basketball"],
+    #     img: "https://www.seadarinn.com/wp-content/uploads/2017/06/ccbl-1.jpg"
+    # },
+    # {
+    #     name: "Matta Field",    
+    #     address: "25 Winslow St. Provincetown, MA", 
+    #     location: {lat: 42.053268, lng: -70.189737}, 
+    #     lat: 42.053268, 
+    #     lng: -70.189737,
+    #     sports: ["baseball", "soccer", "tennis", "basketball"],
+    #     img: "https://provincetown-ma.gov/ImageRepository/Document?documentId=12437"
+    # }
 ])
+        # {
+        #     name: "Brooks Park",    
+        #     address: "1 Oak Street, Harwich, MA", 
+        #     location: {lat: 41.688025, lng: -70.06927}, 
+        #     lat: 41.688025, 
+        #     lng: -70.06927,
+        #     sports: ["basketball", "soccer", "football", "baseball"],
+        #     img: "http://parks.chathamcounty.org/Portals/Parks/Photos/Park%20Photos/Charles%20C%20Brooks%20Park/DSCF0976.jpg"
+        # }
+    # {
+    #     name: "Johnny Kelley",    
+    #     address: "180 Old Bass River Rd, South Dennis, MA", 
+    #     location: {lat: 41.699520, lng: -70.160019}, 
+    #     lat: 41.699520, 
+    #     lng: -70.160019,
+    #     sports: ["basketball", "soccer", "tennis"],
+    #     img: "https://3.bp.blogspot.com/-lDkVJCpt1zI/VbyySsK-8XI/AAAAAAAADLE/a8FRZyUOIYw/s1600/Johnny%2BKelley%2BBasketball%2BCourt.jpg"
+    # },
+    # {
+    #     name: "Old Town House",  
+    #     address: "144 Old Town Hosue Rd, West Yarmouth, MA",      
+    #     location: {lat: 41.684768, lng: -70.216787 },
+    #     lat: 41.684768, 
+    #     lng: -70.216787, 
+    #     sports: ["basketball", "soccer"],
+    #     img: "http://4.bp.blogspot.com/_-_T9WYPXZxQ/TD4xYFeXRwI/AAAAAAAAAr0/qz_nLYsXmY8/s1600/071410_1337.JPG"
+    # },
+    # {
+    #     name: "Ezra Baker",    
+    #     address: "810 Main St, West Dennis, MA 02670", 
+    #     location: {lat: 41.668521, lng: -70.152941}, 
+    #     lat: 41.668521, 
+    #     lng: -70.152941,
+    #     sports: ["basketball", "soccer", "tennis", "baseball"],
+    #     img: "http://www.oldstratforduponavon.com/sitebuilder/images/masscapecod-620x413.jpg"
+    # },
+    # {
+    #     name: "Nathaniel Wixon",    
+    #     address: "901 Rte 134, South Dennis, MA 02660", 
+    #     location: {lat: 41.714379, lng: -70.156675}, 
+    #     lat: 41.714379, 
+    #     lng: -70.156675,
+    #     sports: ["basketball", "baseball", "football", "tennis"],
+    #     img: "http://1.bp.blogspot.com/-dq7MtRfdplo/VhkK5UiS-nI/AAAAAAAADVk/MMS0kSeQjwQ/s1600/Wixon%2BSchool%2BPlaying%2BField.jpg"
+    # }
